e5cfa6f7fe89a139f2c492810624f7aa9ed275fa
Kennet Skov Olesen <kenn799e@edu.campusvejle.dk>
2017-06-01 12:18:23 +0200

    Lavet main med text og billede og time

--- index.php
+++ index.php
@@ -9,46 +9,64 @@
 </head>
 
 <body>
-   <div class="wrapper">
-    <header>
-        <figure class="logo"> <img src="IMG/Del-1-logo%20copy.jpg" alt=""> </figure>
-        <nav>
-            <ul>
-                <li> <a href="#">Forside</a> </li>
-                <li> <a href="#">Games</a> </li>
-                <li> <a href="#">Twitch Stream</a> </li>
-                <li> <a href="#">Work i have done </a> </li>
-                <li> <a href="#">About me</a> </li>
-            </ul>
-        </nav>
-    </header>
-    
-    <main>       
-        <section>
+    <div class="wrapper">
+        <header>
+            <figure class="logo"> <img src="IMG/Del-1-logo%20copy.jpg" alt=""> </figure>
+            <nav>
+                <ul>
+                    <li> <a href="#">Forside</a> </li>
+                    <li> <a href="#">Games</a> </li>
+                    <li> <a href="#">Twitch Stream</a> </li>
+                    <li> <a href="#">Work i have done </a> </li>
+                    <li> <a href="#">About me</a> </li>
+                </ul>
+            </nav>
+        </header>
+        <main>
             <aside>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero architecto similique aspernatur, minima nulla sunt odit? Dignissimos ipsam, deleniti, distinctio sit dolore reiciendis numquam voluptas repellat sed vero in debitis!</p>
+                <div>
+                    <p>LOGIN SYSTEM HER</p>
+                </div>
             </aside>
-            <article>
-               <h2>blablablabla</h2>
-               <img src="IMG/Warhammer%2040k%20billede%201.jpg" alt="" class="img-responsive">
-                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
-            </article>
-            <aside>
-           <div>
-               <p>LOGIN SYSTEM HER</p>
-           </div>
-            </aside>
-        </section>
-        
-<!--        <section> 
+            <section>
+                <article>
+                    <h2>blablablabla</h2> <img src="IMG/Warhammer%2040k%20billede%201.jpg" alt="" class="img-responsive">
+                    <div class="timeStamp">
+                        <time datetime="2009-10-10T19:10-08:00">10oct 2009</time>
+                    </div>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
+                </article>
+                <article>
+                    <h2>totalt mere blablablabla</h2> <img src="IMG/Warhammer%2040k%20billede%202.jpg" alt="" class="img-responsive">
+                    <div class="timeStamp">
+                        <time datetime="2009-10-10T19:10-08:00">10oct 2010</time>
+                    </div>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
+                </article>
+                <article>
+                    <h2>meget mere blablablabla</h2> <img src="IMG/wow%20billede%201.jpg" alt="" class="img-responsive">
+                    <div class="timeStamp">
+                        <time datetime="2009-10-10T19:10-08:00">10oct 2010</time>
+                    </div>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
+                </article>
+                <article>
+                    <h2>lidt mere blablablabla</h2> <img src="IMG/wow%20billede%202.jpg" alt="" class="img-responsive">
+                    <div class="timeStamp">
+                        <time datetime="2009-10-10T19:10-08:00">10oct 2010</time>
+                    </div>
+                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
+                </article>
+            </section>
+            <!--    <section> 
             <img src="IMG/Warhammer%2040k%20billede%201.jpg" alt="">
             <img src="IMG/Warhammer%2040k%20billede%202.jpg" alt=""> 
             <img src="IMG/wow%20billede%201.jpg" alt="">
             <img src="IMG/wow%20billede%202.jpg" alt="">
-        </section>-->
-    </main>
-    
-    <footer>Copyright 2017 Kennet Skov Olesen</footer>
+        </section>
+-->
+        </main>
+        <footer>Copyright 2017 Kennet Skov Olesen</footer>
     </div>
 </body>
 
