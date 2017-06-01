c1e31b8a0589b5387ea4a3e81dde8c23b053185c
Kennet Skov Olesen <kenn799e@edu.campusvejle.dk>
2017-06-01 15:00:42 +0200

    Lavet hele siden

--- index.php
+++ index.php
@@ -17,27 +17,25 @@
                     <li> <a href="#">Forside</a> </li>
                     <li> <a href="#">Games</a> </li>
                     <li> <a href="#">Twitch Stream</a> </li>
-                    <li> <a href="#">Work i have done </a> </li>
+                    <li> <a href="#">Cerberus</a> </li>
                     <li> <a href="#">About me</a> </li>
+                    <li> <a href="#">Log in</a> </li>
                 </ul>
             </nav>
         </header>
         <main>
             <aside>
-                <div>
+                <div class="logIn">
                     <p>LOGIN SYSTEM HER</p>
+                    <form action="/action_page.php">
+                        <input type="text" id="uname" name="UserName" placeholder="User Name..">
+                        <input type="text" id="pword" name="Password" placeholder="Password..">
+                        <input type="submit" value="Submit"> </form>
                 </div>
             </aside>
             <section>
                 <article>
-                    <h2>blablablabla</h2> <img src="IMG/Warhammer%2040k%20billede%201.jpg" alt="" class="img-responsive">
-                    <div class="timeStamp">
-                        <time datetime="2009-10-10T19:10-08:00">10oct 2009</time>
-                    </div>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
-                </article>
-                <article>
-                    <h2>totalt mere blablablabla</h2> <img src="IMG/Warhammer%2040k%20billede%202.jpg" alt="" class="img-responsive">
+                    <h2>lidt mere blablablabla</h2> <img src="IMG/wow%20billede%202.jpg" alt="" class="img-responsive">
                     <div class="timeStamp">
                         <time datetime="2009-10-10T19:10-08:00">10oct 2010</time>
                     </div>
@@ -45,13 +43,6 @@
                 </article>
                 <article>
                     <h2>meget mere blablablabla</h2> <img src="IMG/wow%20billede%201.jpg" alt="" class="img-responsive">
-                    <div class="timeStamp">
-                        <time datetime="2009-10-10T19:10-08:00">10oct 2010</time>
-                    </div>
-                    <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
-                </article>
-                <article>
-                    <h2>lidt mere blablablabla</h2> <img src="IMG/wow%20billede%202.jpg" alt="" class="img-responsive">
                     <div class="timeStamp">
                         <time datetime="2009-10-10T19:10-08:00">10oct 2010</time>
                     </div>
