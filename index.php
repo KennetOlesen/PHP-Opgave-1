d5bbc88944aa45a97355e76bf5fd4c8124ec6a52
Kennet Skov Olesen <kenn799e@edu.campusvejle.dk>
2017-06-01 11:28:24 +0200

    Tilføjede alle filer

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <link rel="stylesheet" href="CSS/main.css">
+    <title>Hjemmeside</title>
+</head>
+
+<body>
+   <div class="wrapper">
+    <header>
+        <figure class="logo"> <img src="IMG/Del-1-logo%20copy.jpg" alt=""> </figure>
+        <nav>
+            <ul>
+                <li> <a href="#">Forside</a> </li>
+                <li> <a href="#">Games</a> </li>
+                <li> <a href="#">Twitch Stream</a> </li>
+                <li> <a href="#">Work i have done </a> </li>
+                <li> <a href="#">About me</a> </li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>       
+        <section>
+            <aside>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero architecto similique aspernatur, minima nulla sunt odit? Dignissimos ipsam, deleniti, distinctio sit dolore reiciendis numquam voluptas repellat sed vero in debitis!</p>
+            </aside>
+            <article>
+               <h2>blablablabla</h2>
+               <img src="IMG/Warhammer%2040k%20billede%201.jpg" alt="" class="img-responsive">
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium minus, maxime ut atque inventore sit vero nihil! Ipsum natus magni nulla fuga, unde suscipit, officia facilis in, libero consequatur asperiores. </p>
+            </article>
+            <aside>
+           <div>
+               <p>LOGIN SYSTEM HER</p>
+           </div>
+            </aside>
+        </section>
+        
+<!--        <section> 
+            <img src="IMG/Warhammer%2040k%20billede%201.jpg" alt="">
+            <img src="IMG/Warhammer%2040k%20billede%202.jpg" alt=""> 
+            <img src="IMG/wow%20billede%201.jpg" alt="">
+            <img src="IMG/wow%20billede%202.jpg" alt="">
+        </section>-->
+    </main>
+    
+    <footer>Copyright 2017 Kennet Skov Olesen</footer>
+    </div>
+</body>
+
+</html>
